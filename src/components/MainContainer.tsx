ae9f3e52d77a4a954759c41aa88d02659199e2ca
Aditya Goel <dtygll@gmail.com>
2026-03-29 19:14:09 +0530

    Add files via upload

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -36,23 +36,19 @@ const MainContainer = ({ children }: PropsWithChildren) => {
       <Navbar />
       <SocialIcons />
       {isDesktopView && children}
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          <div className="container-main">
-            <Landing>{!isDesktopView && children}</Landing>
-            <About />
-            <WhatIDo />
-            <Career />
-            <Work />
-            {isDesktopView && (
-              <Suspense fallback={<div>Loading....</div>}>
-                <TechStack />
-              </Suspense>
-            )}
-            <CaseStudies />
-            <Contact />
-          </div>
-        </div>
+      <div className="container-main">
+        <Landing>{!isDesktopView && children}</Landing>
+        <About />
+        <WhatIDo />
+        <Career />
+        <Work />
+        {isDesktopView && (
+          <Suspense fallback={<div>Loading....</div>}>
+            <TechStack />
+          </Suspense>
+        )}
+        <CaseStudies />
+        <Contact />
       </div>
     </div>
   );
